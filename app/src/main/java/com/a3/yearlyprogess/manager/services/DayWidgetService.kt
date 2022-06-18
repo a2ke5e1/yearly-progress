5642e4bf8b8b855ee6c0171a86407590bbf3aa0c
a2krocks <aakapurv@gmail.com>
2022-06-18 13:26:48 +0530

    Converted into kotlin

--- app/src/main/java/com/a3/yearlyprogess/manager/services/DayWidgetService.kt
+++ app/src/main/java/com/a3/yearlyprogess/manager/services/DayWidgetService.kt
@@ -1,41 +1,29 @@
-package com.a3.yearlyprogess.manager.services;
+package com.a3.yearlyprogess.manager.services
 
-import android.appwidget.AppWidgetManager;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.util.Log;
+import android.content.Intent
+import com.a3.yearlyprogess.manager.WakeLocker
+import com.a3.yearlyprogess.mwidgets.DayWidget
+import android.appwidget.AppWidgetManager
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.util.Log
 
-import com.a3.yearlyprogess.manager.WakeLocker;
-import com.a3.yearlyprogess.mwidgets.DayWidget;
-
-public class DayWidgetService extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
+class DayWidgetService : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
         //wake the device
-        WakeLocker.acquire(context);
-
-        //increase the number in the widget
-        SharedPreferences preferences = context.getSharedPreferences("PREFS", 0);
-        int value = preferences.getInt("value", 1);
-        value++;
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt("value", value);
-        editor.apply();
+        WakeLocker.acquire(context)
 
         //force widget update
-        Intent widgetIntent = new Intent(context, DayWidget.class);
-        widgetIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        int[] ids = AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, DayWidget.class));
-        widgetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
-        context.sendBroadcast(widgetIntent);
-
-        Log.d("WIDGET", "Widget set to update!");
+        val widgetIntent = Intent(context, DayWidget::class.java)
+        widgetIntent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+        val ids = AppWidgetManager.getInstance(context)
+            .getAppWidgetIds(ComponentName(context, DayWidget::class.java))
+        widgetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
+        context.sendBroadcast(widgetIntent)
+        Log.d("WIDGET", "Widget set to update!")
 
         //go back to sleep
-        WakeLocker.release();
+        WakeLocker.release()
     }
 }
