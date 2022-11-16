5aef57b1980e70a9fc3dd2f0c6d43b1c852373f3
a2krocks <aakapurv@gmail.com>
2022-11-16 22:29:15 +0530

    Revert "Fixed Widget Getting Confused which to update"
    This reverts commit 235c221956dcd2a0265e2a76225184256906eb38.

--- app/src/main/java/com/a3/yearlyprogess/manager/services/AllInWidgetService.kt
+++ app/src/main/java/com/a3/yearlyprogess/manager/services/AllInWidgetService.kt
@@ -1,30 +1,11 @@
 package com.a3.yearlyprogess.manager.services
 
-import android.appwidget.AppWidgetManager
-import android.content.BroadcastReceiver
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.a3.yearlyprogess.manager.WakeLocker
-import com.a3.yearlyprogess.mwidgets.AllInWidget
-import com.a3.yearlyprogess.mwidgets.YearWidget
 
-class AllInWidgetService : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        //wake the device
-        WakeLocker.acquire(context)
-
-        //force widget update
-        val widgetIntent = Intent(context, AllInWidget::class.java)
-        widgetIntent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
-        val ids = AppWidgetManager.getInstance(context)
-            .getAppWidgetIds(ComponentName(context, AllInWidget::class.java))
-        widgetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
-        context.sendBroadcast(widgetIntent)
-        // Log.d("WIDGET", "Widget set to update!")
-
-        //go back to sleep
-        WakeLocker.release()
+class AllInWidgetService : BaseWidgetService() {
+    override fun setIntent(context: Context): Intent {
+        return Intent(context, AllInWidgetService::class.java)
     }
+
 }
--- app/src/main/java/com/a3/yearlyprogess/manager/services/MonthWidgetService.kt
+++ app/src/main/java/com/a3/yearlyprogess/manager/services/MonthWidgetService.kt
@@ -1,30 +1,11 @@
 package com.a3.yearlyprogess.manager.services
 
-import android.content.Intent
-import com.a3.yearlyprogess.manager.WakeLocker
-import com.a3.yearlyprogess.mwidgets.DayWidget
-import android.appwidget.AppWidgetManager
-import android.content.BroadcastReceiver
-import android.content.ComponentName
 import android.content.Context
-import android.util.Log
+import android.content.Intent
 import com.a3.yearlyprogess.mwidgets.MonthWidget
 
-class MonthWidgetService : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        //wake the device
-        WakeLocker.acquire(context)
-
-        //force widget update
-        val widgetIntent = Intent(context, MonthWidget::class.java)
-        widgetIntent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
-        val ids = AppWidgetManager.getInstance(context)
-            .getAppWidgetIds(ComponentName(context, MonthWidget::class.java))
-        widgetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
-        context.sendBroadcast(widgetIntent)
-        // Log.d("WIDGET", "Widget set to update!")
-
-        //go back to sleep
-        WakeLocker.release()
+class MonthWidgetService : BaseWidgetService() {
+    override fun setIntent(context: Context): Intent {
+        return Intent(context, MonthWidget::class.java)
     }
 }
