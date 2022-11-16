46f70dffa50f4c4ae3323c4ee20876b496f064cf
a2krocks <aakapurv@gmail.com>
2022-11-16 22:00:57 +0530

    Totally Broken

--- app/src/main/java/com/a3/yearlyprogess/mwidgets/util/StandaloneWidget.kt
+++ app/src/main/java/com/a3/yearlyprogess/mwidgets/util/StandaloneWidget.kt
@@ -17,17 +17,14 @@ import kotlin.math.roundToInt
 
 abstract class StandaloneWidget(private val widgetServiceType: Int) : BaseWidget(widgetServiceType) {
 
-    private lateinit var  widgetType: String
-    private lateinit var widgetCurrentValue: String
-    private lateinit var widgetProgressText: SpannableString
-    private var  widgetProgressBarValue: Int = 0
-
-
-    override fun updateAppWidget(
+    override fun updateWidget(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetId: Int
     ) {
+
+        val view = RemoteViews(context.packageName, R.layout.standalone_widget_layout)
+
         val progressPercentage = ProgressPercentage()
         val progress = progressPercentage.getPercent(
             when (widgetServiceType) {
@@ -39,45 +36,25 @@ abstract class StandaloneWidget(private val widgetServiceType: Int) : BaseWidget
             }
         )
 
-        widgetProgressText = formatProgressStyle(progress)
-        widgetProgressBarValue = progress.roundToInt()
+        val widgetProgressText = formatProgressStyle(progress)
+        val widgetProgressBarValue = progress.roundToInt()
 
-        when (widgetServiceType) {
+        val  widgetType: String = when (widgetServiceType) {
 
-            AlarmHandler.DAY_WIDGET_SERVICE -> {
-                widgetType = context.getString(R.string.day)
-                widgetCurrentValue = progressPercentage.getDay(custom = true)
-            }
-            AlarmHandler.MONTH_WIDGET_SERVICE -> {
-                widgetType = context.getString(R.string.month)
-                widgetCurrentValue = progressPercentage.getMonth(str = true)
-            }
-            AlarmHandler.WEEK_WIDGET_SERVICE -> {
-                widgetType = context.getString(R.string.week)
-                widgetCurrentValue = progressPercentage.getWeek(str = true)
-            }
-            AlarmHandler.YEAR_WIDGET_SERVICE -> {
-                widgetType = context.getString(R.string.year)
-                widgetCurrentValue = progressPercentage.getYear()
-            }
-            else -> {
-                widgetType = ""
-                widgetCurrentValue = ""
-                widgetProgressText = SpannableString("")
-                widgetProgressBarValue = 0
-            }
+            AlarmHandler.DAY_WIDGET_SERVICE -> context.getString(R.string.day)
+            AlarmHandler.MONTH_WIDGET_SERVICE ->  context.getString(R.string.month)
+            AlarmHandler.WEEK_WIDGET_SERVICE ->  context.getString(R.string.week)
+            AlarmHandler.YEAR_WIDGET_SERVICE ->  context.getString(R.string.year)
+            else -> ""
+        }
+        val widgetCurrentValue: String = when(widgetServiceType) {
+            AlarmHandler.DAY_WIDGET_SERVICE -> progressPercentage.getDay(custom = true)
+            AlarmHandler.MONTH_WIDGET_SERVICE -> progressPercentage.getMonth(str = true)
+            AlarmHandler.WEEK_WIDGET_SERVICE -> progressPercentage.getWeek(str = true)
+            AlarmHandler.YEAR_WIDGET_SERVICE -> progressPercentage.getYear()
+            else -> ""
         }
 
-        super.updateAppWidget(context, appWidgetManager, appWidgetId)
-    }
-
-    override fun updateWidget(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
-    ) {
-
-        val view = RemoteViews(context.packageName, R.layout.standalone_widget_layout)
 
         view.setTextViewText(R.id.widgetType, widgetType)
         view.setTextViewText(R.id.widgetCurrentValue, widgetCurrentValue)
