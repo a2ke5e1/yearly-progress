f7a3ba30c038488f1537c4554cc844de31834436
a2krocks <aakapurv@gmail.com>
2022-11-19 19:23:37 +0530

    Fixed All in One Widget not updating

--- app/src/main/java/com/a3/yearlyprogess/manager/services/AllInWidgetService.kt
+++ app/src/main/java/com/a3/yearlyprogess/manager/services/AllInWidgetService.kt
@@ -3,10 +3,11 @@ package com.a3.yearlyprogess.manager.services
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import com.a3.yearlyprogess.mwidgets.AllInWidget
 
 class AllInWidgetService : BaseWidgetService() {
 
-    private val widgetClass = AllInWidgetService::class.java
+    private val widgetClass = AllInWidget::class.java
 
     override fun setIntent(context: Context): Intent {
         return Intent(context, widgetClass)
