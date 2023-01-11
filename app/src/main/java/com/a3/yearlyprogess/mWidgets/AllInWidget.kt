fa16c75c37de33622a4dd6ee4e5530797d38d631
Apurv Ajay Kumar <aakapurv@gmail.com>
2023-01-11 17:30:45 +0530

    Now Widget Preview Will be updated when adding from app

--- app/src/main/java/com/a3/yearlyprogess/mWidgets/AllInWidget.kt
+++ app/src/main/java/com/a3/yearlyprogess/mWidgets/AllInWidget.kt
@@ -21,97 +21,108 @@ import kotlin.math.roundToInt
  * Implementation of App Widget functionality.
  */
 class AllInWidget : BaseWidget(AlarmHandler.ALL_IN_WIDGET_SERVICE) {
+
+    companion object {
+
+        private fun initiateView(context: Context, views: RemoteViews) {
+
+            ProgressPercentage(context).setDefaultWeek()
+
+
+            val dayProgress = ProgressPercentage.getProgress(ProgressPercentage.DAY).roundToInt()
+            val weekProgress = ProgressPercentage.getProgress(ProgressPercentage.WEEK).roundToInt()
+            val monthProgress = ProgressPercentage.getProgress(ProgressPercentage.MONTH).roundToInt()
+            val yearProgress = ProgressPercentage.getProgress(ProgressPercentage.YEAR).roundToInt()
+
+
+
+            views.setTextViewText(R.id.progressTextDay, formatProgress(dayProgress))
+            views.setTextViewText(R.id.progressTextWeek, formatProgress(weekProgress))
+            views.setTextViewText(R.id.progressTextMonth, formatProgress(monthProgress))
+            views.setTextViewText(R.id.progressTextYear, formatProgress(yearProgress))
+
+            views.setProgressBar(R.id.progressBarDay, 100, dayProgress, false)
+            views.setProgressBar(R.id.progressBarWeek, 100, weekProgress, false)
+            views.setProgressBar(R.id.progressBarMonth, 100, monthProgress, false)
+            views.setProgressBar(R.id.progressBarYear, 100, yearProgress, false)
+
+
+            views.setTextViewText(R.id.progressTitle, ProgressPercentage.getDay(formatted = true))
+            views.setTextViewText(
+                R.id.progressWeekTitle,
+                ProgressPercentage.getWeek(isLong = false)
+            )
+            views.setTextViewText(
+                R.id.progressMonthTitle,
+                ProgressPercentage.getMonth(isLong = false)
+            )
+            views.setTextViewText(R.id.progressYearTitle, ProgressPercentage.getYear().toString())
+
+            views.setOnClickPendingIntent(
+                R.id.gridLayout, PendingIntent.getActivity(
+                    context, 0, Intent(context, MainActivity::class.java), PendingIntent.FLAG_IMMUTABLE
+                )
+            )
+
+
+            views.setViewVisibility(R.id.testDay, View.VISIBLE)
+            views.setViewVisibility(R.id.testWeek, View.VISIBLE)
+            views.setViewVisibility(R.id.testMonth, View.VISIBLE)
+            views.setViewVisibility(R.id.testYear, View.VISIBLE)
+        }
+
+
+        fun AllInOneWidgetRemoteView(context: Context): RemoteViews {
+            val small = RemoteViews(context.packageName, R.layout.all_in_widget)
+            val medium = RemoteViews(context.packageName, R.layout.all_in_widget)
+            val large = RemoteViews(context.packageName, R.layout.all_in_widget)
+            val xlarge = RemoteViews(context.packageName, R.layout.all_in_widget)
+
+            initiateView(context, small)
+            initiateView(context, medium)
+            initiateView(context, large)
+            initiateView(context, xlarge)
+
+            small.setViewVisibility(R.id.testWeek, View.GONE)
+            small.setViewVisibility(R.id.testMonth, View.GONE)
+            small.setViewVisibility(R.id.testYear, View.GONE)
+
+            medium.setViewVisibility(R.id.testWeek, View.GONE)
+            medium.setViewVisibility(R.id.testYear, View.GONE)
+
+            large.setViewVisibility(R.id.testWeek, View.GONE)
+
+
+            // Instruct the widget manager to update the widget
+
+            var remoteViews = xlarge
+            if (Build.VERSION.SDK_INT > 30) {
+
+
+                val viewMapping: Map<SizeF, RemoteViews> = mapOf(
+                    SizeF(300f, 80f) to xlarge,
+                    SizeF(220f, 80f) to large,
+                    SizeF(160f, 80f) to medium,
+                    SizeF(100f, 80f) to small,
+                )
+
+                remoteViews = RemoteViews(viewMapping)
+            }
+            return remoteViews
+        }
+    }
+
     override fun updateWidget(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetId: Int
     ) {
-        val small = RemoteViews(context.packageName, R.layout.all_in_widget)
-        val medium = RemoteViews(context.packageName, R.layout.all_in_widget)
-        val large = RemoteViews(context.packageName, R.layout.all_in_widget)
-        val xlarge = RemoteViews(context.packageName, R.layout.all_in_widget)
-
-        initiateView(context, small)
-        initiateView(context, medium)
-        initiateView(context, large)
-        initiateView(context, xlarge)
-
-        small.setViewVisibility(R.id.testWeek, View.GONE)
-        small.setViewVisibility(R.id.testMonth, View.GONE)
-        small.setViewVisibility(R.id.testYear, View.GONE)
-
-        medium.setViewVisibility(R.id.testWeek, View.GONE)
-        medium.setViewVisibility(R.id.testYear, View.GONE)
-
-        large.setViewVisibility(R.id.testWeek, View.GONE)
 
 
-        // Instruct the widget manager to update the widget
-
-        var remoteViews = xlarge
-        if (Build.VERSION.SDK_INT > 30) {
-
-
-            val viewMapping: Map<SizeF, RemoteViews> = mapOf(
-                SizeF(300f, 80f) to xlarge,
-                SizeF(220f, 80f) to large,
-                SizeF(160f, 80f) to medium,
-                SizeF(100f, 80f) to small,
-            )
-
-            remoteViews = RemoteViews(viewMapping)
-        }
-
-        appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+        appWidgetManager.updateAppWidget(appWidgetId, AllInOneWidgetRemoteView(context))
     }
 
 
-    private fun initiateView(context: Context, views: RemoteViews) {
-
-        ProgressPercentage(context).setDefaultWeek()
-
-
-        val dayProgress = ProgressPercentage.getProgress(ProgressPercentage.DAY).roundToInt()
-        val weekProgress = ProgressPercentage.getProgress(ProgressPercentage.WEEK).roundToInt()
-        val monthProgress = ProgressPercentage.getProgress(ProgressPercentage.MONTH).roundToInt()
-        val yearProgress = ProgressPercentage.getProgress(ProgressPercentage.YEAR).roundToInt()
-
-
-
-        views.setTextViewText(R.id.progressTextDay, formatProgress(dayProgress))
-        views.setTextViewText(R.id.progressTextWeek, formatProgress(weekProgress))
-        views.setTextViewText(R.id.progressTextMonth, formatProgress(monthProgress))
-        views.setTextViewText(R.id.progressTextYear, formatProgress(yearProgress))
-
-        views.setProgressBar(R.id.progressBarDay, 100, dayProgress, false)
-        views.setProgressBar(R.id.progressBarWeek, 100, weekProgress, false)
-        views.setProgressBar(R.id.progressBarMonth, 100, monthProgress, false)
-        views.setProgressBar(R.id.progressBarYear, 100, yearProgress, false)
-
-
-        views.setTextViewText(R.id.progressTitle, ProgressPercentage.getDay(formatted = true))
-        views.setTextViewText(
-            R.id.progressWeekTitle,
-            ProgressPercentage.getWeek(isLong = false)
-        )
-        views.setTextViewText(
-            R.id.progressMonthTitle,
-            ProgressPercentage.getMonth(isLong = false)
-        )
-        views.setTextViewText(R.id.progressYearTitle, ProgressPercentage.getYear().toString())
-
-        views.setOnClickPendingIntent(
-            R.id.gridLayout, PendingIntent.getActivity(
-                context, 0, Intent(context, MainActivity::class.java), PendingIntent.FLAG_IMMUTABLE
-            )
-        )
-
-
-        views.setViewVisibility(R.id.testDay, View.VISIBLE)
-        views.setViewVisibility(R.id.testWeek, View.VISIBLE)
-        views.setViewVisibility(R.id.testMonth, View.VISIBLE)
-        views.setViewVisibility(R.id.testYear, View.VISIBLE)
-    }
 
 }
 
--- app/src/main/java/com/a3/yearlyprogess/mWidgets/util/StandaloneWidget.kt
+++ app/src/main/java/com/a3/yearlyprogess/mWidgets/util/StandaloneWidget.kt
@@ -14,63 +14,80 @@ import com.a3.yearlyprogess.helper.ProgressPercentage
 import com.a3.yearlyprogess.manager.AlarmHandler
 import kotlin.math.roundToInt
 
-abstract class StandaloneWidget(private val widgetServiceType: Int) : BaseWidget(widgetServiceType) {
+abstract class StandaloneWidget(private val widgetServiceType: Int) :
+    BaseWidget(widgetServiceType) {
+
+    companion object {
+        fun standaloneWidgetRemoteView(context: Context, widgetServiceType: Int): RemoteViews {
+            val view = RemoteViews(context.packageName, R.layout.standalone_widget_layout)
+            ProgressPercentage(context).setDefaultWeek()
+
+            val progress = ProgressPercentage.getProgress(
+                when (widgetServiceType) {
+                    AlarmHandler.DAY_WIDGET_SERVICE -> ProgressPercentage.DAY
+                    AlarmHandler.MONTH_WIDGET_SERVICE -> ProgressPercentage.MONTH
+                    AlarmHandler.WEEK_WIDGET_SERVICE -> ProgressPercentage.WEEK
+                    AlarmHandler.YEAR_WIDGET_SERVICE -> ProgressPercentage.YEAR
+                    else -> -1
+                }
+            )
+
+            val pref = PreferenceManager.getDefaultSharedPreferences(context)
+            val decimalPlace: Int =
+                pref.getInt(context.getString(R.string.widget_widget_decimal_point), 2)
+
+            val widgetProgressText = formatProgressStyle(
+                SpannableString(
+                    "%,.${decimalPlace}f".format(progress) + "%"
+                )
+            )
+            val widgetProgressBarValue = progress.roundToInt()
+
+            val widgetType: String = when (widgetServiceType) {
+
+                AlarmHandler.DAY_WIDGET_SERVICE -> context.getString(R.string.day)
+                AlarmHandler.MONTH_WIDGET_SERVICE -> context.getString(R.string.month)
+                AlarmHandler.WEEK_WIDGET_SERVICE -> context.getString(R.string.week)
+                AlarmHandler.YEAR_WIDGET_SERVICE -> context.getString(R.string.year)
+                else -> ""
+            }
+            val widgetCurrentValue = when (widgetServiceType) {
+                AlarmHandler.DAY_WIDGET_SERVICE -> ProgressPercentage.getDay(formatted = true)
+                AlarmHandler.MONTH_WIDGET_SERVICE -> ProgressPercentage.getMonth(isLong = false)
+                AlarmHandler.WEEK_WIDGET_SERVICE -> ProgressPercentage.getWeek(isLong = false)
+                AlarmHandler.YEAR_WIDGET_SERVICE -> ProgressPercentage.getYear().toString()
+                else -> ""
+            }
+
+
+            view.setTextViewText(R.id.widgetType, widgetType)
+            view.setTextViewText(R.id.widgetCurrentValue, widgetCurrentValue)
+            view.setTextViewText(R.id.widgetProgress, widgetProgressText)
+            view.setProgressBar(R.id.widgetProgressBar, 100, widgetProgressBarValue, false)
+
+            view.setOnClickPendingIntent(
+                android.R.id.background, PendingIntent.getActivity(
+                    context,
+                    0,
+                    Intent(context, MainActivity::class.java),
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+            )
+
+            return view
+        }
+    }
+
 
     override fun updateWidget(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetId: Int
     ) {
-
-        val view = RemoteViews(context.packageName, R.layout.standalone_widget_layout)
-        ProgressPercentage(context).setDefaultWeek()
-
-        val progress = ProgressPercentage.getProgress(
-            when (widgetServiceType) {
-                AlarmHandler.DAY_WIDGET_SERVICE -> ProgressPercentage.DAY
-                AlarmHandler.MONTH_WIDGET_SERVICE -> ProgressPercentage.MONTH
-                AlarmHandler.WEEK_WIDGET_SERVICE -> ProgressPercentage.WEEK
-                AlarmHandler.YEAR_WIDGET_SERVICE -> ProgressPercentage.YEAR
-                else -> -1
-            }
+        appWidgetManager.updateAppWidget(
+            appWidgetId,
+            standaloneWidgetRemoteView(context, widgetServiceType)
         )
-
-        val pref = PreferenceManager.getDefaultSharedPreferences(context)
-        val decimalPlace: Int = pref.getInt(context.getString(R.string.widget_widget_decimal_point), 2)
-
-        val widgetProgressText = formatProgressStyle(SpannableString(
-            "%,.${decimalPlace}f".format(progress) + "%"
-        ))
-        val widgetProgressBarValue = progress.roundToInt()
-
-        val  widgetType: String = when (widgetServiceType) {
-
-            AlarmHandler.DAY_WIDGET_SERVICE -> context.getString(R.string.day)
-            AlarmHandler.MONTH_WIDGET_SERVICE ->  context.getString(R.string.month)
-            AlarmHandler.WEEK_WIDGET_SERVICE ->  context.getString(R.string.week)
-            AlarmHandler.YEAR_WIDGET_SERVICE ->  context.getString(R.string.year)
-            else -> ""
-        }
-        val widgetCurrentValue = when(widgetServiceType) {
-            AlarmHandler.DAY_WIDGET_SERVICE -> ProgressPercentage.getDay(formatted = true)
-            AlarmHandler.MONTH_WIDGET_SERVICE -> ProgressPercentage.getMonth(isLong = false)
-            AlarmHandler.WEEK_WIDGET_SERVICE -> ProgressPercentage.getWeek(isLong = false)
-            AlarmHandler.YEAR_WIDGET_SERVICE -> ProgressPercentage.getYear().toString()
-            else -> ""
-        }
-
-
-        view.setTextViewText(R.id.widgetType, widgetType)
-        view.setTextViewText(R.id.widgetCurrentValue, widgetCurrentValue)
-        view.setTextViewText(R.id.widgetProgress, widgetProgressText)
-        view.setProgressBar(R.id.widgetProgressBar, 100,widgetProgressBarValue, false)
-
-        view.setOnClickPendingIntent(android.R.id.background, PendingIntent.getActivity(
-            context, 0, Intent(context, MainActivity::class.java), PendingIntent.FLAG_IMMUTABLE
-        ))
-
-        appWidgetManager.updateAppWidget(appWidgetId, view)
-
     }
 
 }
