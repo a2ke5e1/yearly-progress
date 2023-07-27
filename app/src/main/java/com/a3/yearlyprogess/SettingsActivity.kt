a5858d3389382d6e26e8a8e20878f8a13b487c6f
a2krocks <aakapurv@gmail.com>
2023-07-27 16:02:36 +0530

    Fixed Settings page not edge-to-edge

--- app/src/main/java/com/a3/yearlyprogess/SettingsActivity.kt
+++ app/src/main/java/com/a3/yearlyprogess/SettingsActivity.kt
@@ -3,9 +3,15 @@ package com.a3.yearlyprogess
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
@@ -21,6 +27,20 @@ class SettingsActivity : AppCompatActivity() {
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        val appBarLayout: AppBarLayout = findViewById(R.id.appBarLayout)
+
+
+        window.navigationBarDividerColor =
+            ContextCompat.getColor(this, android.R.color.transparent)
+        window.navigationBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        ViewCompat.setOnApplyWindowInsetsListener(appBarLayout) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = insets.top)
+            WindowInsetsCompat.CONSUMED
+        }
 
         toolbar.setNavigationOnClickListener {
             finish()
