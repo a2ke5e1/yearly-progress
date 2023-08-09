25e2f6c916818c04d6d0606235dabfe861e8d9ff
a2krocks <aakapurv@gmail.com>
2023-08-09 13:31:08 +0530

    Added animations

--- app/src/main/java/com/a3/yearlyprogess/components/CustomEventCardView.kt
+++ app/src/main/java/com/a3/yearlyprogess/components/CustomEventCardView.kt
@@ -6,17 +6,14 @@ import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.text.SpannableString
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.preference.PreferenceManager
 import com.a3.yearlyprogess.R
 import com.a3.yearlyprogess.databinding.CustomEventCardViewBinding
 import com.a3.yearlyprogess.eventManager.model.Event
 import com.a3.yearlyprogess.helper.ProgressPercentage.Companion.formatProgressStyle
 import com.a3.yearlyprogess.helper.ProgressPercentage
-import com.google.android.material.card.MaterialCardView
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
@@ -34,6 +31,7 @@ class CustomEventCardView @JvmOverloads constructor(
 
     private var binding: CustomEventCardViewBinding =
         CustomEventCardViewBinding.inflate(LayoutInflater.from(context), this, true)
+    private val settingsPref = PreferenceManager.getDefaultSharedPreferences(context)
 
     private var job: Job
     private var event: Event? = null
@@ -41,16 +39,12 @@ class CustomEventCardView @JvmOverloads constructor(
 
     init {
         orientation = VERTICAL
-
-        ProgressPercentage(context).setDefaultWeek()
-        ProgressPercentage(context).setDefaultCalculationMode()
-
         job = Job()
-
     }
 
     fun setEvent(event: Event) {
         this.event = event
+
 
         binding.eventTitle.text = event.eventTitle
         binding.eventTitle.text = event.eventTitle
@@ -63,13 +57,27 @@ class CustomEventCardView @JvmOverloads constructor(
             event.eventEndTime
         )
 
-        val settingsPref = PreferenceManager.getDefaultSharedPreferences(context)
-        val decimalPlace: Int =
-            settingsPref.getInt(context.getString(R.string.widget_event_widget_decimal_point), 2)
-
 
         launch(Dispatchers.IO) {
+
+            val progress = ProgressPercentage.getProgress(
+                ProgressPercentage.CUSTOM_EVENT,
+                event.eventStartTime,
+                event.eventEndTime
+            )
+
+            launch(Dispatchers.Main) {
+                updateView(progress)
+            }
+
             while (true) {
+
+                val decimalPlace: Int =
+                    settingsPref.getInt(context.getString(R.string.widget_event_widget_decimal_point), 2)
+
+                ProgressPercentage(context).setDefaultWeek()
+                ProgressPercentage(context).setDefaultCalculationMode()
+
                 val progress = ProgressPercentage.getProgress(
                     ProgressPercentage.CUSTOM_EVENT,
                     event.eventStartTime,
@@ -89,6 +97,37 @@ class CustomEventCardView @JvmOverloads constructor(
             }
         }
 
+    }
+
+    private fun updateView(progress: Double) {
+
+        val decimalPlace: Int = settingsPref.getInt(context.getString(R.string.widget_event_widget_decimal_point), 2)
+
+
+
+        // val params = widgetProgressCard.layoutParams
+        // val target = (progress * 0.01 * widgetParentCard.width).toInt()
+        val progressBarValueAnimator = ValueAnimator.ofInt(0, progress.toInt())
+        val progressTextValueAnimator = ValueAnimator.ofFloat(0F, progress.toFloat())
+
+        progressBarValueAnimator.duration = 500
+        progressTextValueAnimator.duration = 500
+
+        progressBarValueAnimator.addUpdateListener {
+            binding.progressBar.progress = it.animatedValue as Int
+            binding.progressBar.requestLayout()
+        }
+
+        progressTextValueAnimator.addUpdateListener {
+            binding.progressText.text = formatProgressStyle(
+                SpannableString(
+                    "%,.${decimalPlace}f".format(it.animatedValue) + "%"
+                )
+            )
+        }
+
+        progressBarValueAnimator.start()
+        progressTextValueAnimator.start()
     }
 
 
