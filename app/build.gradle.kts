c2b3269b2e6e2d9fa350758967ac7ce364dd8bd9
a2krocks <aakapurv@gmail.com>
2023-08-20 03:27:42 +0530

    version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.a3.yearlyprogess"
         minSdk = 28
         targetSdk = 34
-        versionCode = 46
+        versionCode = 47
 
         /*
         Version:
@@ -23,7 +23,7 @@ android {
         New Widgets Styles - x.3.x.x
         */
         val localVersionCode = versionCode
-        versionName = "1.2.1.${localVersionCode!! - 46}"
+        versionName = "1.2.1.${localVersionCode!! - 47}"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
