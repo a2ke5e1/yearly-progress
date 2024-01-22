ba158b6068a17158f83fd127a3837e8a01395c23
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-01-22 18:59:55 +0530

    upgraded version code to 49

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.a3.yearlyprogess"
         minSdk = 28
         targetSdk = 34
-        versionCode = 48
+        versionCode = 49
 
         /*
         Version:
