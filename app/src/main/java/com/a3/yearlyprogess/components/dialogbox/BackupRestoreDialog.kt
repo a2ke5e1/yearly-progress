2df4b7b9e4b6a751d24b2943a116f0440d9fdcae
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-01-31 00:54:31 +0530

    Fixed few issues

--- app/src/main/java/com/a3/yearlyprogess/components/dialogbox/BackupRestoreDialog.kt
+++ app/src/main/java/com/a3/yearlyprogess/components/dialogbox/BackupRestoreDialog.kt
@@ -23,13 +23,15 @@ class BackupRestoreDialog(private val roomBackup: RoomBackup) : DialogFragment()
 
         roomBackup.database(EventDatabase.getDatabase(requireContext())).enableLogDebug(true)
             .backupLocation(RoomBackup.BACKUP_FILE_LOCATION_CUSTOM_DIALOG)
+            .backupIsEncrypted(true)
+            .customEncryptPassword("haha idc if you forgot")
             .apply {
                 onCompleteListener { success, message, exitCode ->
                     when (exitCode) {
                         RoomBackupCodes.EXIT_CODE_ERROR_STORAGE_PERMISSONS_NOT_GRANTED -> {
                             this@BackupRestoreDialog.dismiss()
                             MaterialAlertDialogBuilder(context, R.style.CentralCard)
-                                .setIcon(R.drawable.ic_round_settings_24)
+                                .setIcon(R.drawable.ic_storage)
                                 .setTitle("Storage Permission")
                                 .setMessage("To ensure a seamless experience with our backup & restore feature, we kindly request access to your device's storage. Granting this permission allows us to securely safeguard your data during backups and swiftly restore it when needed. Your privacy and data security are our utmost priorities. Thank you for entrusting us with the protection of your valuable information")
                                 .setNeutralButton("Okay") { _, _ ->
@@ -43,12 +45,12 @@ class BackupRestoreDialog(private val roomBackup: RoomBackup) : DialogFragment()
                         )
                     }
                     if (success) {
-                        restartApp(Intent(requireContext(), MainActivity::class.java))
                         Toast.makeText(
                             requireContext(),
-                            "Backup was successful.",
+                            "Operation was successful.",
                             Toast.LENGTH_LONG
                         ).show()
+                        restartApp(Intent(requireContext(), MainActivity::class.java))
                     }
                 }
             }
