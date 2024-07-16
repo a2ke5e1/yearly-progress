decd2bc3c951369b934531a565ac0c4cad4e9996
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-07-16 13:12:39 +0530

    chore: Add a check to prevent start of foreground service in android 15 and above on boot up

--- app/src/main/java/com/a3/yearlyprogess/widgets/manager/updateManager/services/AutoStartService.kt
+++ app/src/main/java/com/a3/yearlyprogess/widgets/manager/updateManager/services/AutoStartService.kt
@@ -3,6 +3,8 @@ package com.a3.yearlyprogess.widgets.manager.updateManager.services;
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.util.Log
 import com.a3.yearlyprogess.widgets.manager.updateManager.services.WidgetUpdateService
 
 // This service will be started on boot up
@@ -11,6 +13,10 @@ class AutoStartService : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val action = intent.action
         if (action == Intent.ACTION_BOOT_COMPLETED || action.equals("com.a3.yearlyprogress.RESTART_WIDGET_UPDATE_SERVICE")) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                Log.d("AutoStartService", "Starting foreground service is not supported on android 15 and above")
+                return
+            }
             val serviceIntent = Intent(context, WidgetUpdateService::class.java)
             context.startForegroundService(serviceIntent)
         }
