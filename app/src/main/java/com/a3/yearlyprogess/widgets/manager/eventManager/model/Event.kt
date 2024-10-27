cc2499549c00d8f811c98e35c2ee6482051c8a52
a2ke5e1 <aakapurv@gmail.com>
2024-10-27 19:23:43 +0530

    Test #1: Yearly Event Schedule is working

--- app/src/main/java/com/a3/yearlyprogess/widgets/manager/eventManager/model/Event.kt
+++ app/src/main/java/com/a3/yearlyprogess/widgets/manager/eventManager/model/Event.kt
@@ -4,6 +4,8 @@ import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
+import java.util.Calendar
+import kotlin.math.abs
 
 @Parcelize
 @Entity(tableName = "event_table")
@@ -15,7 +17,75 @@ data class Event(
     val eventStartTime: Long,
     val eventEndTime: Long,
     val repeatEventDays: List<RepeatDays> = emptyList(),
-) : Parcelable
+) : Parcelable {
+    fun nextStartAndEndTime(currentTime: Long = System.currentTimeMillis()): Pair<Long, Long> {
+        var nextStartTime = eventStartTime
+        var nextEndTime = eventEndTime
+
+        // If event has not completed, return current start and end time
+        // Do not need to calculate next start and end time
+        if (currentTime < nextEndTime) {
+            return Pair(nextStartTime, nextEndTime)
+        }
+
+        // If event is not repeating, return current start and end time
+        // Do not need to calculate next start and end time
+        if (repeatEventDays.isEmpty()) {
+            return Pair(nextStartTime, nextEndTime)
+        }
+
+        // If user has selected to repeat event every year, move to next year to start and end time
+        if (repeatEventDays.contains(RepeatDays.EVERY_YEAR)) {
+            val currCalendar = Calendar.getInstance()
+            val eventCalendar = Calendar.getInstance()
+
+            currCalendar.timeInMillis = currentTime
+            eventCalendar.timeInMillis = nextStartTime
+
+            var diffYear = abs(currCalendar.get(Calendar.YEAR) - eventCalendar.get(Calendar.YEAR))
+            if (currentTime.getMonth() > nextEndTime.getMonth()) {
+                diffYear += 1
+            }
+
+            eventCalendar.add(Calendar.YEAR, diffYear)
+            nextStartTime = eventCalendar.timeInMillis
+
+            eventCalendar.timeInMillis = nextEndTime
+            eventCalendar.add(Calendar.YEAR, diffYear)
+            nextEndTime = eventCalendar.timeInMillis
+
+        }
+
+        if (repeatEventDays.contains(RepeatDays.EVERY_MONTH)) {
+            val currCalendar = Calendar.getInstance()
+            val eventCalendar = Calendar.getInstance()
+
+            currCalendar.timeInMillis = System.currentTimeMillis()
+            eventCalendar.timeInMillis = nextStartTime
+
+            val diffMonth = eventCalendar.get(Calendar.MONTH) - currCalendar.get(Calendar.MONTH)
+
+            eventCalendar.add(Calendar.MONTH, diffMonth)
+            nextStartTime = eventCalendar.timeInMillis
+
+            eventCalendar.timeInMillis = nextEndTime
+            eventCalendar.add(Calendar.MONTH, diffMonth)
+            nextEndTime = eventCalendar.timeInMillis
+
+        }
+
+
+        return Pair(nextStartTime, nextEndTime)
+    }
+
+    companion object {
+        private fun Long.getMonth(): Int {
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = this
+            return calendar.get(Calendar.MONTH)
+        }
+    }
+}
 
 // Enum class for days event will be repeated
 enum class RepeatDays {
--- app/src/test/java/com/a3/yearlyprogess/ExampleUnitTest.kt
+++ app/src/test/java/com/a3/yearlyprogess/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.a3.yearlyprogess
 
+import com.a3.yearlyprogess.widgets.manager.eventManager.model.Event
+import com.a3.yearlyprogess.widgets.manager.eventManager.model.RepeatDays
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.util.Calendar
+import java.util.Date
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +14,106 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun repeat_every_year_one_year() {
+        val cal = Calendar.getInstance()
+        cal.set(2022, 11, 1, 0, 0, 0)
+        var currentTime = cal.timeInMillis
+
+        cal.set(2022, 9, 1, 0, 0, 0)
+        val start = cal.timeInMillis
+        cal.set(2022, 9, 1, 1, 0, 0)
+        val end = cal.timeInMillis
+
+        cal.set(2023, 9, 1, 0, 0, 0)
+        val nextStart = cal.timeInMillis
+        cal.set(2023, 9, 1, 1, 0, 0)
+        val nextEnd = cal.timeInMillis
+
+        val event = Event(
+            1,
+            "Event 1",
+            "Event 1 Description",
+            false,
+            start,
+            end,
+            listOf(RepeatDays.EVERY_YEAR)
+        )
+
+        val nextSchedule = event.nextStartAndEndTime(
+            currentTime = currentTime
+        )
+
+        assertEquals(Date(nextStart), Date(nextSchedule.first))
+        assertEquals(Date(nextEnd), Date(nextSchedule.second))
     }
+
+    @Test
+    fun repeat_every_year_five_years_case_1() {
+        val cal = Calendar.getInstance()
+        cal.set(2027, 4, 1, 0, 0, 0)
+        var currentTime = cal.timeInMillis
+
+        cal.set(2022, 9, 1, 0, 0, 0)
+        val start = cal.timeInMillis
+        cal.set(2022, 9, 1, 1, 0, 0)
+        val end = cal.timeInMillis
+
+        cal.set(2027, 9, 1, 0, 0, 0)
+        val nextStart5 = cal.timeInMillis
+        cal.set(2027, 9, 1, 1, 0, 0)
+        val nextEnd5 = cal.timeInMillis
+
+        val event = Event(
+            1,
+            "Event 1",
+            "Event 1 Description",
+            false,
+            start,
+            end,
+            listOf(RepeatDays.EVERY_YEAR)
+        )
+
+        val nextSchedule5 = event.nextStartAndEndTime(
+            currentTime = currentTime
+        )
+
+        assertEquals(Date(nextStart5), Date(nextSchedule5.first))
+        assertEquals(Date(nextEnd5), Date(nextSchedule5.second))
+    }
+
+    @Test
+    fun repeat_every_year_five_years_case_2() {
+        val cal = Calendar.getInstance()
+        cal.set(2025, 3, 1, 0, 0, 0)
+        var currentTime = cal.timeInMillis
+
+        cal.set(2022, 9, 1, 0, 0, 0)
+        val start = cal.timeInMillis
+        cal.set(2022, 9, 1, 1, 0, 0)
+        val end = cal.timeInMillis
+
+        cal.set(2025, 9, 1, 0, 0, 0)
+        val nextStart6 = cal.timeInMillis
+        cal.set(2025, 9, 1, 1, 0, 0)
+        val nextEnd6 = cal.timeInMillis
+
+        val event = Event(
+            1,
+            "Event 1",
+            "Event 1 Description",
+            false,
+            start,
+            end,
+            listOf(RepeatDays.EVERY_YEAR)
+        )
+
+        val nextSchedule6 = event.nextStartAndEndTime(
+            currentTime = currentTime
+        )
+
+        assertEquals(Date(nextStart6), Date(nextSchedule6.first))
+        assertEquals(Date(nextEnd6), Date(nextSchedule6.second))
+    }
+
+
 }
