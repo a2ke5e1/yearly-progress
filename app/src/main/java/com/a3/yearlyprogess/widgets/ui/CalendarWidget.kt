08dbe5a66690a81e08326092c2bad5e5299d4c0b
a2ke5e1 <aakapurv@gmail.com>
2025-01-13 17:26:07 +0530

    UI Demo #2

--- app/src/main/java/com/a3/yearlyprogess/widgets/ui/CalendarWidget.kt
+++ app/src/main/java/com/a3/yearlyprogess/widgets/ui/CalendarWidget.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
+import android.util.SizeF
 import android.widget.RemoteViews
 import com.a3.yearlyprogess.R
 import com.a3.yearlyprogess.calculateProgress
@@ -13,7 +15,6 @@ import com.a3.yearlyprogess.widgets.manager.CalendarEventInfo.getSelectedCalenda
 import com.a3.yearlyprogess.widgets.manager.eventManager.model.Event
 import com.a3.yearlyprogess.widgets.ui.util.styleFormatted
 import com.a3.yearlyprogess.widgets.ui.util.toTimePeriodText
-import java.text.DateFormat
 import java.util.Date
 import kotlin.math.roundToInt
 
@@ -24,50 +25,27 @@ class CalendarWidget : BaseWidget() {
       appWidgetId: Int
   ) {
 
-    val view = RemoteViews(context.packageName, R.layout.calendar_widget_layout)
+    val smallView = RemoteViews(context.packageName, R.layout.calendar_widget_small_layout)
+    val largeView = RemoteViews(context.packageName, R.layout.calendar_widget_layout)
+
     if (context.checkSelfPermission(Manifest.permission.READ_CALENDAR) ==
         PackageManager.PERMISSION_DENIED) {
-      emptyWidget(view)
-      view.setTextViewText(R.id.event_title, "Calendar permission required")
+      emptyWidget(smallView)
+      emptyWidget(largeView)
+      smallView.setTextViewText(R.id.event_title, "Calendar permission required")
+      largeView.setTextViewText(R.id.event_title, "Calendar permission required")
+      val view = mapRemoteView(context, appWidgetId, smallView, largeView)
       appWidgetManager.updateAppWidget(appWidgetId, view)
       return
     }
 
-    /*var text = ""
-        val selectedCalendars = getSelectedCalendarIds(context)
-        if (selectedCalendars.isNullOrEmpty()) {
-          text = "No calendars selected"
-        } else {
-          text = "Selected calendars: ${selectedCalendars.joinToString(", ")}"
-        }
-
-        if (selectedCalendars != null) {
-          for (calendarId in selectedCalendars) {
-            text += "\n\n"
-            val event = getCurrentEventOrUpcomingEvent(context.contentResolver, calendarId)
-            text += "Calendar: $calendarId\n"
-            if (event != null) {
-              text += event.eventTitle
-              text += "\n"
-              text += event.eventDescription.take(20)
-              text += "\n"
-              text += event.eventStartTime
-              text += "\n"
-              text += event.eventEndTime
-              text += "\n"
-              text += calculateProgress(context, event.eventStartTime.time, event.eventEndTime.time).styleFormatted(2)
-            } else {
-              text += "No upcoming events"
-            }
-          }
-        }
-
-        view.setTextViewText(R.id.text, text)
-    */
     val selectedCalendars = getSelectedCalendarIds(context)
     if (selectedCalendars.isNullOrEmpty()) {
-      emptyWidget(view)
-      view.setTextViewText(R.id.event_title, "No calendars selected")
+      emptyWidget(smallView)
+      emptyWidget(largeView)
+      smallView.setTextViewText(R.id.event_title, "No calendars selected")
+      largeView.setTextViewText(R.id.event_title, "No calendars selected")
+      val view = mapRemoteView(context, appWidgetId, smallView, largeView)
       appWidgetManager.updateAppWidget(appWidgetId, view)
       return
     }
@@ -81,9 +59,11 @@ class CalendarWidget : BaseWidget() {
     }
 
     if (events.isEmpty()) {
-      emptyWidget(view)
-      view.setTextViewText(R.id.event_title, "No upcoming events")
-      appWidgetManager.updateAppWidget(appWidgetId, view)
+      emptyWidget(smallView)
+      emptyWidget(largeView)
+      smallView.setTextViewText(R.id.event_title, "No upcoming events")
+      largeView.setTextViewText(R.id.event_title, "No upcoming events")
+      appWidgetManager.updateAppWidget(appWidgetId, smallView)
       return
     }
 
@@ -91,6 +71,41 @@ class CalendarWidget : BaseWidget() {
         events
             .filter { event -> event.eventEndTime.time > System.currentTimeMillis() }
             .minBy { event -> event.eventStartTime }
+
+    setupCalendarWidgetView(context, smallView, event)
+    setupCalendarWidgetView(context, largeView, event)
+
+    val view = mapRemoteView(context, appWidgetId, smallView, largeView)
+    appWidgetManager.updateAppWidget(appWidgetId, view)
+  }
+
+  fun mapRemoteView(
+      context: Context,
+      widgetId: Int,
+      smallView: RemoteViews,
+      largeViews: RemoteViews
+  ): RemoteViews {
+    if (Build.VERSION.SDK_INT > 30) {
+      val viewMapping: Map<SizeF, RemoteViews> =
+          mapOf(
+              SizeF(60f, 140f) to smallView,
+              SizeF(130f, 140f) to largeViews,
+          )
+      return RemoteViews(viewMapping)
+    }
+
+    val option = AppWidgetManager.getInstance(context).getAppWidgetOptions(widgetId)
+    val height = option.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
+    val width = option.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
+
+    if (height >= 100 && width >= 100) {
+      return largeViews
+    }
+
+    return smallView
+  }
+
+  fun setupCalendarWidgetView(context: Context, view: RemoteViews, event: Event) {
 
     val progress = calculateProgress(context, event.eventStartTime.time, event.eventEndTime.time)
     view.setTextViewText(
@@ -101,7 +116,11 @@ class CalendarWidget : BaseWidget() {
     view.setTextViewText(R.id.event_description, event.eventDescription)
     view.setTextViewText(
         R.id.event_duration,
-        "${event.eventStartTime.formattedDateTime(context)} — ${event.eventEndTime.formattedDateTime(context)}")
+        "${event.eventStartTime.formattedDateTime(context)} — ${
+        event.eventEndTime.formattedDateTime(
+          context
+        )
+      }")
     view.setProgressBar(R.id.widgetProgressBar, 100, progress.roundToInt(), false)
 
     val widgetDays =
@@ -123,8 +142,6 @@ class CalendarWidget : BaseWidget() {
     if (event.eventDescription.isEmpty()) {
       view.setViewVisibility(R.id.event_description, android.view.View.GONE)
     }
-
-    appWidgetManager.updateAppWidget(appWidgetId, view)
   }
 
   private fun emptyWidget(view: RemoteViews) {
