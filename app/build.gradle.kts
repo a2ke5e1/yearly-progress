b4d8773c3d6692acc16c1e8623fbc76993f34182
a2ke5e1 <aakapurv@gmail.com>
2025-07-12 17:24:15 +0530

    Migrated Material3Expressive to Material3
    The Material3Expressive library is deprecated. This commit updates the app to use the standard Material3 library.
    
    Additionally, dependencies were updated and the target SDK was bumped to 36.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,20 +6,20 @@ plugins {
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
-    id("com.ncorti.ktfmt.gradle") version "0.21.0"
+    id("com.ncorti.ktfmt.gradle") version "0.23.0"
     id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.a3.yearlyprogess"
         minSdk = 30
-        targetSdk = 35
-        versionCode = 135
-        versionName = "2.16.0-beta07"
+        targetSdk = 36
+        versionCode = 136
+        versionName = "2.16.0-beta08"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -58,53 +58,53 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.0")
-    implementation("com.google.firebase:firebase-crashlytics:19.4.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.1")
+    implementation("com.google.firebase:firebase-crashlytics:19.4.4")
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.work:work-runtime-ktx:2.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.work:work-runtime-ktx:2.10.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.06.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.accompanist:accompanist-permissions:0.37.2")
-    implementation("androidx.compose.ui:ui-tooling:1.8.1")
-  implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
+    implementation("androidx.compose.ui:ui-tooling:1.8.3")
+  implementation("androidx.navigation:navigation-compose:2.9.1")
   testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // RecyclerView Selection
-    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+    implementation("androidx.recyclerview:recyclerview-selection:1.2.0")
 
     // Material Design 3.0
-    implementation("com.google.android.material:material:1.13.0-alpha13")
+    implementation("com.google.android.material:material:1.13.0-beta01")
 
     // Splash Screen for android 11 and below
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Ads
-    implementation("com.google.android.gms:play-services-ads:24.2.0")
+    implementation("com.google.android.gms:play-services-ads:24.4.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.1")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Room
-    val roomVersion = "2.7.1"
+    val roomVersion = "2.7.2"
 
     implementation("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
@@ -112,10 +112,10 @@ dependencies {
     androidTestImplementation("androidx.room:room-testing:$roomVersion")
 
     // Room backup and restore
-    implementation("de.raphaelebner:roomdatabasebackup:1.0.2")
+    implementation("de.raphaelebner:roomdatabasebackup:1.1.0")
 
     // Lifecycle Components
-    val lifecycleVersion = "2.9.0"
+    val lifecycleVersion = "2.9.1"
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
@@ -127,9 +127,9 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
 }
