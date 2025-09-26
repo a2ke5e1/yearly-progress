595ede8f669a36c76ea4ac09eaebb580ab2006a9
Apurv Kumar <aakapurv@gmail.com>
2025-09-27 00:11:49 +0530

    Added new progress card with refresh rate of 1ms

--- app/src/main/java/com/a3/yearlyprogess/MainActivity.kt
+++ app/src/main/java/com/a3/yearlyprogess/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,36 +20,41 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.a3.yearlyprogess.ui.components.ProgressCard
 import com.a3.yearlyprogess.ui.theme.YearlyProgressTheme
 
+
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    installSplashScreen()
-    enableEdgeToEdge()
-    setContent {
-      YearlyProgressTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          LazyColumn(
-            contentPadding = innerPadding,
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-          ) {
-            item { ProgressCard(timePeriod = TimePeriod.YEAR, refreshInterval = 200) }
-            item { ProgressCard(timePeriod = TimePeriod.WEEK) }
-            item { ProgressCard(timePeriod = TimePeriod.MONTH) }
-            item { ProgressCard(timePeriod = TimePeriod.DAY, refreshInterval = 100) }
-          }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        enableEdgeToEdge()
+        setContent {
+            YearlyProgressTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets.safeDrawing
+                ) { innerPadding ->
+                    LazyColumn(
+                        modifier = Modifier.padding(8.dp, 0.dp),
+                        contentPadding = innerPadding,
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                    ) {
+                        item { ProgressCard(timePeriod = TimePeriod.YEAR) }
+                        item { ProgressCard(timePeriod = TimePeriod.MONTH) }
+                        item { ProgressCard(timePeriod = TimePeriod.WEEK) }
+                        item { ProgressCard(timePeriod = TimePeriod.DAY) }
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(text = "Hello $name!", modifier = modifier)
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-  YearlyProgressTheme { Greeting("Android") }
+    YearlyProgressTheme { Greeting("Android") }
 }
--- app/src/main/java/com/a3/yearlyprogess/ui/components/ProgressCard.kt
+++ app/src/main/java/com/a3/yearlyprogess/ui/components/ProgressCard.kt
@@ -1,38 +1,49 @@
 package com.a3.yearlyprogess.ui.components
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.a3.yearlyprogess.*
+import com.a3.yearlyprogess.ProgressSettings
+import com.a3.yearlyprogess.TimePeriod
+import com.a3.yearlyprogess.YearlyProgressUtil
 import kotlinx.coroutines.delay
 
 @Composable
 fun ProgressCard(
     timePeriod: TimePeriod,
     settings: ProgressSettings = ProgressSettings(),
-    modifier: Modifier = Modifier,
-    decimals: Int = 6,
-    refreshInterval: Long = 200L
+    modifier: Modifier = Modifier, decimals: Int = 13, refreshInterval: Long = 1L
 ) {
     val progressUtil = remember { YearlyProgressUtil(settings) }
 
     val startTime = progressUtil.calculateStartTime(timePeriod)
     val endTime = progressUtil.calculateEndTime(timePeriod)
+    val duration = (endTime - startTime) / 1000
 
     var progress by remember { mutableStateOf(progressUtil.calculateProgress(startTime, endTime)) }
 
     LaunchedEffect(Unit) {
-        while (progress < 100.0) {
+        while (true) {
             progress = progressUtil.calculateProgress(startTime, endTime)
             delay(refreshInterval)
         }
@@ -41,35 +52,68 @@ fun ProgressCard(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(140.dp)
-            .background(
-                brush = Brush.horizontalGradient(listOf(Color(0xFF4CAF50), Color(0xFF81C784))),
-                shape = RoundedCornerShape(16.dp)
-            ),
-        contentAlignment = Alignment.Center
+            .height(160.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainer)
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Box(
+            modifier = modifier
+                .fillMaxWidth((progress / 100).toFloat().coerceIn(0f, 1f))
+                .fillMaxHeight()
+                .background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .align(Alignment.CenterStart)
+        )
+
+        Column(
+            modifier = Modifier.padding(18.dp), horizontalAlignment = Alignment.Start
+        ) {
             Text(
-                text = when (settings.calculationType) {
-                    CalculationType.ELAPSED -> "Elapsed"
-                    CalculationType.REMAINING -> "Remaining"
+                text = timePeriod.name.lowercase().replaceFirstChar { it.uppercase() },
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.onSurface,
+            )
+            Text(
+                text = when (timePeriod) {
+                    TimePeriod.MONTH -> progressUtil.getMonthName(
+                        progressUtil.getCurrentPeriodValue(
+                            timePeriod
+                        )
+                    )
+
+                    TimePeriod.WEEK -> progressUtil.getWeekDayName(
+                        progressUtil.getCurrentPeriodValue(
+                            timePeriod
+                        )
+                    )
+
+                    TimePeriod.DAY -> progressUtil.getCurrentPeriodValue(timePeriod)
+                        .toString() + progressUtil.getOrdinalSuffix(
+                        progressUtil.getCurrentPeriodValue(
+                            timePeriod
+                        )
+                    )
+
+                    else -> progressUtil.getCurrentPeriodValue(timePeriod).toString()
                 },
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Medium,
-                color = Color.White
+                color = MaterialTheme.colorScheme.onSurface,
             )
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = "%.${decimals}f%%".format(progress), // update directly using Double
                 fontSize = 32.sp,
                 fontWeight = FontWeight.Bold,
-                color = Color.White
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
             )
-            Spacer(modifier = Modifier.height(4.dp))
             Text(
-                text = "${timePeriod.name.lowercase().replaceFirstChar { it.uppercase() }} Progress",
+                text = "of ${duration}s",
                 fontSize = 14.sp,
-                color = Color.White.copy(alpha = 0.9f)
+                color = MaterialTheme.colorScheme.onSurface,
             )
         }
     }
