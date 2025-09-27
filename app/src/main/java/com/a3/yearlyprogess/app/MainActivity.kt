9314c24324203c948eca51d71fe67b80e260e027
Apurv Kumar <aakapurv@gmail.com>
2025-09-27 17:06:24 +0530

    fix: Fixed padding issues in mobile and tablet view

--- app/src/main/java/com/a3/yearlyprogess/app/MainActivity.kt
+++ app/src/main/java/com/a3/yearlyprogess/app/MainActivity.kt
@@ -4,32 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.a3.yearlyprogess.app.navigation.AppNavGraph
-import com.a3.yearlyprogess.app.navigation.AppNavigationDrawer
 import com.a3.yearlyprogess.app.navigation.AppNavigationRail
 import com.a3.yearlyprogess.app.navigation.BottomNavigationBar
-import com.a3.yearlyprogess.feature.home.ui.components.ProgressCard
 import com.a3.yearlyprogess.core.ui.theme.YearlyProgressTheme
-import com.a3.yearlyprogess.core.util.TimePeriod
-import com.a3.yearlyprogess.feature.home.ui.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -56,9 +44,13 @@ class MainActivity : ComponentActivity() {
                     else -> {
                         // Foldable / Tablet
                         Scaffold { innerPadding ->
-                            Row(Modifier.fillMaxSize()) {
+                            Row(
+                                modifier = Modifier
+                                    .padding(innerPadding)
+                                    .fillMaxSize(),
+                            ) {
                                 AppNavigationRail(navController)
-                                AppNavGraph(navController, Modifier.padding(innerPadding))
+                                AppNavGraph(navController)
                             }
                         }
                     }
--- app/src/main/java/com/a3/yearlyprogess/feature/home/ui/HomeScreen.kt
+++ app/src/main/java/com/a3/yearlyprogess/feature/home/ui/HomeScreen.kt
@@ -1,12 +1,9 @@
 package com.a3.yearlyprogess.feature.home.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,19 +12,17 @@ import com.a3.yearlyprogess.feature.home.ui.components.ProgressCard
 
 @Composable
 fun HomeScreen() {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        contentWindowInsets = WindowInsets.safeDrawing
-    ) { innerPadding ->
+
         LazyColumn(
-            modifier = Modifier.padding(8.dp, 0.dp),
-            contentPadding = innerPadding,
+            modifier = Modifier.padding(8.dp, 0.dp).fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             item { ProgressCard(timePeriod = TimePeriod.YEAR) }
             item { ProgressCard(timePeriod = TimePeriod.MONTH) }
             item { ProgressCard(timePeriod = TimePeriod.WEEK) }
             item { ProgressCard(timePeriod = TimePeriod.DAY) }
+            item { ProgressCard(timePeriod = TimePeriod.DAY) }
+            item { ProgressCard(timePeriod = TimePeriod.DAY) }
         }
-    }
+
 }
