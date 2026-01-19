8e653c3bcff0dcae73926b8ef452a795b04bcfb9
Apurv Kumar <aakapurv@gmail.com>
2026-01-19 22:00:01 +0530

    Bump version to 4.0.0-alpha16 and update version code to 215

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.a3.yearlyprogess"
         minSdk = 30
         targetSdk = 36
-        versionCode = 214
-        versionName = "4.0.0-alpha15"
+        versionCode = 215
+        versionName = "4.0.0-alpha16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
