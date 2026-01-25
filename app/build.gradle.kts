92f9178b530a8bed999bd1644073e3781dbdcab9
Apurv Kumar <aakapurv@gmail.com>
2026-01-25 17:54:09 +0530

    reconfig gradle to support gradle 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
@@ -27,8 +26,9 @@ fun getEnvProperty(key: String, required: Boolean = false): String {
 
 android {
     namespace = "com.a3.yearlyprogess"
-    compileSdk = 36
-
+    compileSdk {
+        version = release(36)
+    }
     defaultConfig {
         applicationId = "com.a3.yearlyprogess"
         minSdk = 30
@@ -71,9 +71,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+
     buildFeatures {
         compose = true
         buildConfig = true
+        resValues = true
     }
 }
 
