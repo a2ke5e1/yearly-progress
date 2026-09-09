65f802636491d8d79bcd134a2b9b1c7ebdab24b5
Apurv Kumar <aakapurv@gmail.com>
2026-09-09 17:38:26 +0530

    chore: Bump app version to 4.1.0 (245)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.a3.yearlyprogess"
         minSdk = 30
         targetSdk = 37
-        versionCode = 244
-        versionName = "4.0.15"
+        versionCode = 245
+        versionName = "4.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
